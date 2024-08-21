165be84da3cac2b244eb05b3bc14fff53dd1ddf8
Amit_C4 <casam@post.bgu.ac.il>
2024-08-21 16:17:30 +0300

    first commit

---
+++ app/src/test/java/hw3_prj/App.java
@@ -0,0 +1,11 @@
+package hw3_prj;
+
+public class App {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+
+}
--- app/src/test/java/hw3_prj/AppTest.java
+++ app/src/test/java/hw3_prj/AppTest.java
@@ -6,8 +6,10 @@ package hw3_prj;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
-    @Test void appHasAGreeting() {
+
+public class AppTest {
+    @Test
+    public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
