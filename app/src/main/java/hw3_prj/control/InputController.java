c2051870208600704b6c1252eed41ab6654cadb5
Amit_C4 <casam@post.bgu.ac.il>
2024-08-21 16:25:29 +0300

    fix scanner problem

--- app/src/main/java/hw3_prj/control/InputController.java
+++ app/src/main/java/hw3_prj/control/InputController.java
@@ -34,41 +34,41 @@ public class InputController {
     
 
     public Player choosePlayer() {
-        Scanner scanner = new Scanner(System.in);
-        msg_Callback.send("Select Player:");
-        TileFactory tileFactory = TileFactory.getInstance(msg_Callback);
+        try (Scanner scanner = new Scanner(System.in)) {
+            msg_Callback.send("Select Player:");
+            TileFactory tileFactory = TileFactory.getInstance(msg_Callback);
 
-        for (int i = 1; i <= 7; i++)
-            msg_Callback.send(i + ". " + tileFactory.producePlayer(i, new Position(0, 0)).getName());
+            for (int i = 1; i <= 7; i++)
+                msg_Callback.send(i + ". " + tileFactory.producePlayer(i, new Position(0, 0)).getName());
 
-        int id = scanner.nextInt();
-        while (id < 1 || id > 7) {
-            msg_Callback.send("Invalid Player ID. Please select a valid Player ID: ");
-            id = scanner.nextInt();
+            int id = scanner.nextInt();
+            while (id < 1 || id > 7) {
+                msg_Callback.send("Invalid Player ID. Please select a valid Player ID: ");
+                id = scanner.nextInt();
+            }
+
+            Player player = tileFactory.producePlayer(id, new Position(0, 0));
+            msg_Callback.send("You have selected:\n " + player.getName());
+            return player;
         }
-
-        Player player = tileFactory.producePlayer(id, new Position(0, 0));
-        msg_Callback.send("You have selected:\n " + player.getName());
-        return player;
     }
 
     public InputType getAction() {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            msg_Callback.send("Enter movement (W,A,S,D,E,Q): ");
 
-        msg_Callback.send("Enter movement (W,A,S,D,E,Q): ");
-
-        char inputChar = scanner.next().charAt(0);
-
-        inputChar = Character.toLowerCase(inputChar);
-        InputType res = inputTypes.get(inputChar);
-
-        while (res == null) {
-            msg_Callback.send("Invalid Input. Please enter a valid input.");
-            inputChar = scanner.next().charAt(0);
+            char inputChar = scanner.next().charAt(0);
             inputChar = Character.toLowerCase(inputChar);
-            res = inputTypes.get(inputChar);
+            InputType res = inputTypes.get(inputChar);
+
+            while (res == null) {
+                msg_Callback.send("Invalid Input. Please enter a valid input.");
+                inputChar = scanner.next().charAt(0);
+                inputChar = Character.toLowerCase(inputChar);
+                res = inputTypes.get(inputChar);
+            }
+            return res;
         }
-        return res;
     }
 
     public InputType getAltAction(){
